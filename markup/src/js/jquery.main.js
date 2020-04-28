998fd6b2534f3aa62d1b5d4572904dfe34dbe3ed
mykhail.s <mykhail.s@codeproduction.co>
2020-04-28 18:18:45 +0300

    process

--- markup/src/js/jquery.main.js
+++ markup/src/js/jquery.main.js
@@ -1,7 +1,39 @@
 jQuery(function () {
    $(document).foundation();
-
+   initAnimate();
 });
+
+
+function initAnimate() {
+   var condition = $('.animateBlock').size()
+   // && false
+   ;
+   init(condition);
+
+   function init(condition) {
+       if (condition || condition == null) {
+           $('.animateBlock').viewportChecker({
+               classToAdd: 'animate_start',
+               offset: '10%'
+           });
+       }
+   }
+}
+
+function initFaqsAccordion() {
+   var condition = $('.faqAccordion').size()
+   // && false
+   ;
+   init(condition);
+
+   function init(condition) {
+       if (condition || condition == null) {
+
+         
+
+       }
+   }
+}
 
  //= plugins.js
 
