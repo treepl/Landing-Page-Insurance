158b96779496bde913e608b69718f81033223e42
mykhail.s <mykhail.s@codeproduction.co>
2020-04-29 18:25:08 +0300

    process

--- markup/src/js/jquery.main.js
+++ markup/src/js/jquery.main.js
@@ -1,6 +1,8 @@
 jQuery(function () {
    $(document).foundation();
    initAnimate();
+   initFaqsAccordion();
+   initTestimonialCarousel();
 });
 
 
@@ -28,11 +30,46 @@ function initFaqsAccordion() {
 
    function init(condition) {
        if (condition || condition == null) {
-
+        jQuery('.faqAccordion .faqs-open-close').openClose({
+            activeClass: 'active',
+            opener: '.opener',
+            slider: '.slide',
+            animSpeed: 400,
+            effect: 'slide'
+        });
          
 
        }
    }
+}
+
+function initTestimonialCarousel() {
+
+    var condition = $('.testimonialSliderHolder').size()
+    // && false
+    ;
+    init(condition);
+  
+    function init(condition) {
+        if (condition || condition == null) {
+  
+            $('.testimonialSliderHolder').slick({
+                prevArrow: '<button class="slick-prev" aria-label="Previous" type="button" tabindex="0"><i class="icon icon-left-arrow"></i> </button>',
+                nextArrow: '<button class="slick-next" aria-label="Next" type="button" tabindex="0"><i class="icon icon-right-arrow"></i></button>',
+                centerMode: true,
+                slidesToShow: 3,
+                responsive: [
+                    {
+                      breakpoint: 860,
+                      settings: {
+                        slidesToShow: 1,
+                      }
+                    }
+                  ]
+            });
+            
+        }   
+    }
 }
 
  //= plugins.js
